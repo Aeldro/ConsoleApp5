08c9b89e267b452da1e66566c67de4807ba6c119
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-06 11:19:26 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static float Add(float x, float y)
+            {
+                return x + y;
+            }
+
+            static float Substract(float x, float y)
+            {
+                return x - y;
+            }
+
+            static float Multiply(float x, float y)
+            {
+                return x + y;
+            }
+
+            static float Divide(float x, float y)
+            {
+                return x + y;
+            }
+
+            static int Modulo(int x, int y)
+            {
+                return x + y;
+            }
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
