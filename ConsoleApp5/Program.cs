3c11a9d4345053f68a26c6273f8c42ead096d5fb
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-06 12:06:25 +0200

    Done calc

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -16,21 +16,50 @@
 
             static float Multiply(float x, float y)
             {
-                return x + y;
+                return x * y;
             }
 
             static float Divide(float x, float y)
             {
-                return x + y;
+                return x / y;
             }
 
             static int Modulo(int x, int y)
             {
-                return x + y;
+                return x % y;
             }
 
             while (true)
             {
+                Console.WriteLine("Enter a number.");
+                float firstNumber = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter another number.");
+                float secondNumber = float.Parse(Console.ReadLine());
+                Console.WriteLine("Chose an operation. (add, substract, multiply, divide, modulo)");
+                string operation = Console.ReadLine();
+
+                if (operation == "add")
+                {
+                    float result = Add(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+                } else if (operation == "substract") { 
+                    float result = Substract(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
+                } else if (operation == "multiply")
+                {
+                    float result = Multiply(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} x {secondNumber} = {result}");
+                } else if (operation == "divide") {
+                    float result = Divide(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} / {secondNumber} = {result}");
+                } else if (operation == "modulo")
+                {
+                    float result = Modulo(Convert.ToInt32(firstNumber), Convert.ToInt32(secondNumber));
+                    Console.WriteLine($"{firstNumber} % {secondNumber} = {result}");
+                } else
+                {
+                    Console.WriteLine("Wrong operation.");
+                }
 
             }
         }
